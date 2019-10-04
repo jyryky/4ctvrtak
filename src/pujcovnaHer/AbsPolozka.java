e2a11c904bc18a89c5cc3f087e76510d9ff2a23d
Oldrich Havelka <oldrichhavelka80085@gmail.com>
2019-10-04 08:35:21 +0200

    Very commited to this relationship

--- src/pujcovnaHer/AbsPolozka.java
+++ src/pujcovnaHer/AbsPolozka.java
@@ -1,4 +1,22 @@
 package pujcovnaHer;
 
-public class AbsPolozka {
+public abstract class AbsPolozka {
+
+    private String jmeno;
+    private String cena;
+    private boolean pujceno;
+    private String jmenoPujcitele;
+
+    public AbsPolozka(){
+        this.jmeno=jmeno;
+        this.cena=cena;
+        this.pujceno=pujceno;
+        this.jmenoPujcitele=jmenoPujcitele;
+    }
+
+    public abstract void pujcit();
+    public abstract void vratit();
+    public abstract void vytvorit();
+    public abstract void znicit();
+
 }
