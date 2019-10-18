5b4f12f03dbbd4c8b9f070d71b83819ed888c290
Oldrich Havelka <oldrichhavelka80085@gmail.com>
2019-10-18 07:58:42 +0200

    help

--- src/pujcovnaHer/AbsPolozka.java
+++ src/pujcovnaHer/AbsPolozka.java
@@ -3,20 +3,23 @@ package pujcovnaHer;
 public abstract class AbsPolozka {
 
     private String jmeno;
-    private String cena;
+    private int cena;
     private boolean pujceno;
     private String jmenoPujcitele;
+    private String vyvojar;
 
     public AbsPolozka(){
         this.jmeno=jmeno;
         this.cena=cena;
         this.pujceno=pujceno;
         this.jmenoPujcitele=jmenoPujcitele;
+        this.vyvojar=vyvojar;
     }
 
     public abstract void pujcit();
     public abstract void vratit();
     public abstract void vytvorit();
     public abstract void znicit();
+    public abstract void zmenitCenu();
 
 }
--- src/pujcovnaHer/Platformer.java
+++ src/pujcovnaHer/Platformer.java
@@ -1,4 +1,24 @@
 package pujcovnaHer;
 
 public class Platformer extends AbsPolozka {
+
+    private String jmeno;
+    private int cena;
+    private boolean pujceno;
+    private String jmenoPujcitele;
+    private String vyvojar;
+
+    public Platformer(String jmeno, int cena, boolean pujceno, String jmenoPujcitele, String vyvojar) {
+        this.jmeno = jmeno;
+        this.cena = cena;
+        this.pujceno = pujceno;
+        this.jmenoPujcitele = jmenoPujcitele;
+        this.vyvojar = vyvojar;
+    }
+
+    public void pujcit(){}
+    public void vratit(){}
+    public void vytvorit(){}
+    public void znicit(){}
+    public void zmenitCenu(){}
 }
