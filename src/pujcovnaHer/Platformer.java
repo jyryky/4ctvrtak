529a7d80f8e5272e15287df2674122da324a99e1
Oldrich Havelka <oldrichhavelka80085@gmail.com>
2019-10-18 08:11:23 +0200

    help

--- src/pujcovnaHer/Platformer.java
+++ src/pujcovnaHer/Platformer.java
@@ -2,18 +2,9 @@ package pujcovnaHer;
 
 public class Platformer extends AbsPolozka {
 
-    private String jmeno;
-    private int cena;
-    private boolean pujceno;
-    private String jmenoPujcitele;
-    private String vyvojar;
 
     public Platformer(String jmeno, int cena, boolean pujceno, String jmenoPujcitele, String vyvojar) {
-        this.jmeno = jmeno;
-        this.cena = cena;
-        this.pujceno = pujceno;
-        this.jmenoPujcitele = jmenoPujcitele;
-        this.vyvojar = vyvojar;
+        super();
     }
 
     public void pujcit(){}
